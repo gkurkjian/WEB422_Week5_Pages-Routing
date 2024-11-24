aef374556b88408e6306ebef97d9e81ae3501fc3
George K <gkurkjian@myseneca.ca>
2024-11-23 21:34:40 -0500

    second commit, included files, folders/files, will interpret them in main page on next commit

---
+++ my-app/pages/dashboard/index.js
@@ -0,0 +1,7 @@
+export default function Dashboard() {  // Just to you know, notice here we didn't call the function name same as the file
+    return (
+        <>
+            Dashboard
+        </>
+    );
+}
